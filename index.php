5161b3da82be44fc43ddd94298dfd35a2f1b8476
Antha <antax666@gmail.com>
2020-07-23 10:37:45 +0800

    My 2 Tier

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 	<title>Webcam</title>
 	<script src="machine/html2canvas.js"></script>
-	<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <body>
 	<div class="video-wrap" style="float:left" >
